63acf22fa58bc8e9009f7e199431fca8a5de3520
qahor0v <fluttuzgroup@gmail.com>
2023-08-10 06:17:04 +0500

    changed project architecture

--- README.md
+++
@@ -1,4 +0,0 @@
-## E-Learning app
-* Android
-* iOS
-
--- android/app/src/main/kotlin/com/example/education/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.education
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
